02d3fed821a69c5b3577e242d7fbf54cbdeff86a
Fasikaw Kindye <fasikawkn@gmail.com>
2025-02-12 00:40:29 +0300

    Add support for custom delimiters in StringCalculator

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -8,6 +8,16 @@ class StringCalculator {
       return int.parse(numbers);
     }
 
+    // Check for a custom delimiter at the start
+    if (numbers.startsWith('//')) {
+      final delimiterLineEnd = numbers.indexOf('\n');
+      final delimiter = numbers.substring(2, delimiterLineEnd);
+      numbers = numbers.substring(delimiterLineEnd + 1);
+
+      // Replace new delimiter and split numbers
+      numbers = numbers.replaceAll(delimiter, ',');
+    }
+
     // Replace teh new line character with a comma and split the string
     List<String> numberList = numbers.replaceAll('\n', ',').split(',');
 
