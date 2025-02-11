833992e1a3fcb8cc750d987de710960aa94abb59
Fasikaw Kindye <fasikawkn@gmail.com>
2025-02-12 00:30:09 +0300

    Add test for single number input in StringCalculator

--- test/string_caculator_test.dart
+++ test/string_caculator_test.dart
@@ -3,7 +3,12 @@ import 'package:test/test.dart';
 
 void main() {
   StringCalculator stringCalculator = StringCalculator();
+
   test('empty string returns 0', () {
     expect(stringCalculator.add(''), equals(0));
+  });
+
+  test('single number returns the same number', () {
+    expect(stringCalculator.add('1'), equals(1));
   });
 }
