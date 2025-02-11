8004a4da9778da03ce1d0781241bdba3358e193f
Fasikaw Kindye <fasikawkn@gmail.com>
2025-02-12 00:25:31 +0300

    Implement StringCalculator class with add method to handle empty string input

--- test/string_caculator_test.dart
+++ test/string_caculator_test.dart
@@ -1,8 +1,9 @@
+import 'package:string_caculator/string_calculator.dart';
 import 'package:test/test.dart';
 
 void main() {
   StringCalculator stringCalculator = StringCalculator();
   test('empty string returns 0', () {
-    expect(StringCalculator.add(''), equals(0));
+    expect(stringCalculator.add(''), equals(0));
   });
 }
